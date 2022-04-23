1655ed1a592941dc0f6f22915345b7ad362ede19
Mitarbeiter-Admin <adm@nbimp017.ost.ch>
2022-04-23 17:31:35 +0200

    initial commit

---
+++ src/first_test/ice_wheel_arduino/ice_wheel_arduino.ino
@@ -0,0 +1,29 @@
+int aoutPin = 33;
+int i = 0;
+int speed_step = 5;
+int pause = 500;
+bool dir = true;
+
+void setup() {
+  pinMode(aoutPin, OUTPUT);
+
+}
+
+void loop() {
+  if(dir){
+    i = i + speed_step;
+  } else {
+    i = i - speed_step;
+  }
+    
+  if(i < 50){
+    dir = true;
+  }
+
+  if(i > 200){
+    dir = false;
+  }
+  
+  analogWrite(aoutPin, i);
+  delay(pause);
+}
